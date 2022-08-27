4e0abdb38048664673166bffcac16608ff25e257
Manuel Schneider <scmanuel@ethz.ch>
2022-08-27 09:02:57 +0200

    Simplified file and removed unnecessary lines

--- examples/study.cases/diffPhysics/run-burger-nn.py
+++ examples/study.cases/diffPhysics/run-burger-nn.py
@@ -30,11 +30,11 @@ ic      = 'sinus'
 noise   = 0.
 seed    = 42
 
-dns =  Burger_jax(L=L, N=N,  dt=dt,   nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
-sgs0 = Burger_jax(L=L, N=N2, dt=dt*s, nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
+dns = Burger_jax(L=L, N=N,  dt=dt,   nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
+sgs = Burger_jax(L=L, N=N2, dt=dt*s, nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
 
 v0 = np.concatenate((dns.v0[:((N2+1)//2)], dns.v0[-(N2-1)//2:]))
-sgs0.IC( v0 = v0 * N2 / N )
+sgs.IC( v0 = v0 * N2 / N )
 
 #------------------------------------------------------------------------------
 print("Simulate DNS ..")
@@ -44,10 +44,8 @@ dns.compute_Ek()
 
 print("Simulate SGS..")
 # simulate
-sgs0.simulate()
-# convert to physical space
-sgs0.compute_Ek()
-sgs = sgs0
+sgs.simulate()
+sgs.compute_Ek()
 
 #------------------------------------------------------------------------------
 print(dns.uu[-1])
