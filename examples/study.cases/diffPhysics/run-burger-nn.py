2516c0f2e9caa6b6bf3fb17723218a23d5520f3a
Manuel Schneider <scmanuel@ethz.ch>
2022-08-27 09:40:11 +0200

    Fixed file and prepared the necessary steps to couple it to the neural network

--- examples/study.cases/diffPhysics/run-burger-nn.py
+++ examples/study.cases/diffPhysics/run-burger-nn.py
@@ -1,8 +1,4 @@
-#!/bin/python3
-
-# Discretization grid
-N = 512
-N2 = 32
+# GET REFERENCE SOLUTION AND LOW RESOLUTION SOLUTION TO BE TRAINED
 
 import matplotlib
 matplotlib.use('Agg')
@@ -13,14 +9,20 @@ import argparse
 sys.path.append('./_model/')
 
 import numpy as np
-from plotting_jax import *
-from Burger_jax import *
+from plotting import *
+from Burger import *
 
 #------------------------------------------------------------------------------
-## set parameters and initialize simulation
+# Initialization
+
+# Discetization grid
+N = 1024
+N2 = 32
+
+# Set parameters
 L       = 2*pi
 dt      = 0.001
-s       = N/N2
+s       = 32
 tEnd    = 100
 nu      = 0.02
 #ic      = 'zero'
@@ -29,23 +31,40 @@ ic      = 'sinus'
 #ic      = 'forced'
 noise   = 0.
 seed    = 42
+forcing = True
 
-dns = Burger_jax(L=L, N=N,  dt=dt,   nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
-sgs = Burger_jax(L=L, N=N2, dt=dt*s, nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed)
+# Initialize both versions
+dns = Burger(L=L, N=N,  dt=dt,   nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed, forcing=forcing, s=s)
+sgs = Burger(L=L, N=N2, dt=dt*s, nu=nu, tend=tEnd, case=ic, noise=noise, seed=seed, forcing=forcing, s=s)
 
 v0 = np.concatenate((dns.v0[:((N2+1)//2)], dns.v0[-(N2-1)//2:]))
 sgs.IC( v0 = v0 * N2 / N )
 
+sgs.randfac1 = dns.randfac1
+sgs.randfac2 = dns.randfac2
+
 #------------------------------------------------------------------------------
+# Perform simulations for both versions
+
 print("Simulate DNS ..")
-# simulate
+## simulate
 dns.simulate()
 dns.compute_Ek()
 
 print("Simulate SGS..")
-# simulate
+## simulate
 sgs.simulate()
 sgs.compute_Ek()
 
+
 #------------------------------------------------------------------------------
-print(dns.uu[-1])
+# Store solutions
+dns_sol = dns.uu
+sgs_sol = sgs.uu
+print(sgs_sol[-1])
+
+
+
+# TODO: IMPLEMENT TRAINING FROM neural-network-baseline.py
+
+
