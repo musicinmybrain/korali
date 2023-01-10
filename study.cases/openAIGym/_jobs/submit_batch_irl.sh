899eb39041bd02c729ee51c11e2921454e013ee9
wadaniel <wadaniel@ethz.ch>
2023-01-10 14:34:00 +0100

    minor

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,16 +1,17 @@
-run=1200
+run=1600
 
 export ENV="Swimmer-v4"
 export POL="Linear"
-export EXP=3000000
+export EXP=5000000
+export EBRU=5000
 
 #for D in 1 4 16 64
-for D in 4 16
+for D in 4 16 64
 do 
     for B in 4 16
-#    for B in 1 4 16 64
+    #for B in 1 4 16 64
     do 
-        for rep in {1..1}
+        for rep in {1..2}
         do
             run=$(($run+1))
             export RUN=$run
