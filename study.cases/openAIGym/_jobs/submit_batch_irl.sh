4eabb5c0cbe50935b801108f694498a2493689cf
wadaniel <wadaniel@ethz.ch>
2023-01-16 16:43:03 +0100

    obs

--- study.cases/openAIGym/_jobs/submit_batch_irl.sh
+++ study.cases/openAIGym/_jobs/submit_batch_irl.sh
@@ -1,17 +1,17 @@
-run=1600
+run=2000
 
-export ENV="Swimmer-v4"
+export ENV="HalfCheetah-v4"
 export POL="Linear"
-export EXP=5000000
+export EXP=3000000
 export EBRU=5000
 
-#for D in 1 4 16 64
 for D in 4 16 64
+#for D in 4 16 64
 do 
-    for B in 4 16
-    #for B in 1 4 16 64
+    #for B in 4 16
+    for B in 4 16 64
     do 
-        for rep in {1..2}
+        for rep in {1..1}
         do
             run=$(($run+1))
             export RUN=$run
